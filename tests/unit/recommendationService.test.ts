acc9553034fd48cd2170270f1b12e289118f7736
amontheape <mcanedotome@gmail.com>
2022-05-09 05:46:09 -0300

    unit-test: upvote and downvote notFoundError

--- tests/unit/recommendationService.test.ts
+++ tests/unit/recommendationService.test.ts
@@ -5,11 +5,33 @@ import * as errors from "../../src/utils/errorUtils.js"
 
 describe("recommendation service test suit", () => {
   it("GET /recommendations/random should throw not_found when there are no recs", () => {
-    jest.spyOn(Math, "random").mockReturnValue(1)
+    const mockedMathValue = 0.5
+
+    jest.spyOn(Math, "random").mockReturnValue(mockedMathValue)
     jest.spyOn(recommendationRepository, "findAll").mockResolvedValue([])
 
     expect( async () => {
       await recommendationService.getRandom()
+    }).rejects.toEqual(errors.notFoundError())
+  })
+
+  it("POST /recommendations/:id/upvote should throw not_found given invalid ID", () => {
+    const testID = 1
+
+    jest.spyOn(recommendationRepository, "find").mockResolvedValue(null)
+
+    expect( async () => {
+      await recommendationService.upvote(testID) 
+    }).rejects.toEqual(errors.notFoundError())
+  })
+
+  it("POST /recommendations/:id/downvote should throw not_found given invalid ID", () => {
+    const testID = 1
+
+    jest.spyOn(recommendationRepository, "find").mockResolvedValue(null)
+
+    expect( async () => {
+      await recommendationService.downvote(testID) 
     }).rejects.toEqual(errors.notFoundError())
   })
 })
